2119b378bc4d743317cde8aef0b01c00ad159e51
Charles McGarvey <chazmcgarvey@brokenzipper.com>
2019-06-13 02:54:36 -0600

    fix bug making --version, --help flags unusable

--- lib/App/HomeBank2Ledger.pm
+++ lib/App/HomeBank2Ledger.pm
@@ -66,6 +66,10 @@ sub main {
     if ($opts->{manual}) {
         pod2usage(-exitval => 0, -verbose => 2);
     }
+    if (!$opts->{input}) {
+        print STDERR "Input file is required.\n";
+        exit(1);
+    }
 
     my $homebank = File::HomeBank->new(file => $opts->{input});
 
@@ -400,10 +404,6 @@ sub parse_args {
     ) or pod2usage(-exitval => 1, -verbose => 99, -sections => [qw(SYNOPSIS OPTIONS)]);
 
     $opts{input} = shift @args if !$opts{input};
-    if (!$opts{input}) {
-        print STDERR "Input file is required.\n";
-        exit(1);
-    }
 
     return \%opts;
 }
